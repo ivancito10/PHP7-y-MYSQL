2f3c7909c6b196064757750204635d680c3e8891
ivancito10 <50561919+ivancito10@users.noreply.github.com>
2021-05-17 19:43:50 -0400

    Create hola_mundo.php

---
+++ curso_php/hola_mundo.php
@@ -0,0 +1,18 @@
+<?php
+//echo nos sirve para mostrar informacion en la pantalla
+#otro comentario
+/* otro mas */
+//echo "Hola Mundo";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1><? echo "Hola Mundo"; ?></h1>
+</body>
+</html>
